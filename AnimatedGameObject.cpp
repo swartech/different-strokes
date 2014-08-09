2b36dd121ea1de4c90961d0148d1b9a1c49ed026
PringleLiam <pringle.liam@yahoo.co.uk>
2014-08-09 14:44:01 +0100

    Updated Animated Object

--- AnimatedGameObject.cpp
+++ AnimatedGameObject.cpp
@@ -1,6 +1,6 @@
 #include "AnimatedGameObject.h"
-#include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
+
 class AnimatedGameObject {
 
 sf::Texture spriteSheet;
@@ -8,9 +8,11 @@ int frameCount;
 int currentFrame;
 
 
-AnimatedGameObject::AnimatedGameObject(sf::Texture spriteSheet, int width, int height, int currentFrame, sf::Vector2f position, sf::Vector2f velocity, bool collidable)
+AnimatedGameObject::AnimatedGameObject(sf::Texture spritesheet, int width, int height, int currentframe, sf::Vector2f position, sf::Vector2f velocity, bool collidable):GameObject(width,height,position,velocity,collidable)
 {
-
+ 
+ spriteSheet = spritesheet;
+ currentFrame = currentframe;
 }
 
  
